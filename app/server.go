5bed6417a50c20e355541aed3937da1875883494
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2023-03-25 00:38:33 +0200

    no log statement

--- app/server.go
+++ app/server.go
@@ -12,7 +12,6 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	fmt.Println("Listening on port 6379")
 	_, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
